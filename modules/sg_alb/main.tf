6b32c22c65d02e9e50efb2f9cadf7c4ee866d13e
AkalankaDilshan <akalankadilshan98@gmail.com>
2026-02-12 17:13:38 +0530

    feat: complete ALB sg

---
+++ modules/sg_alb/main.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "alb_sg" {
+  name = var.alb_sg_name
+  description = "Security Group for ALB and allow HTTPS nad HTTP"
+  vpc_id =  var.vpc_id 
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.alb_sg_name
+  }
+}
---
+++ modules/sg_alb/variables.tf
@@ -0,0 +1,9 @@
+variable "alb_sg_name" {
+  description = "sg name"
+  type = string
+}
+
+variable "vpc_id" {
+  description = "main vpc id"
+  type = string
+}
