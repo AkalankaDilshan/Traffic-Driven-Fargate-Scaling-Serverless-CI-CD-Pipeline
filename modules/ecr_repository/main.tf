7a95b538f83f4f72a2194e83832ff80a445e67f1
AkalankaDilshan <akalankadilshan98@gmail.com>
2026-02-13 11:32:11 +0530

    refactor: complete ECR module

---
+++ modules/ecr_repository/main.tf
@@ -0,0 +1,23 @@
+resource "aws_ecr_repository" "app_repo" {
+  name = var.ecr_repo_name
+
+  image_tag_mutability = "MUTABLE_WITH_EXCLUSION"
+
+  image_tag_mutability_exclusion_filter {
+    filter_type = "WILDCARD"
+    filter      = "v*"
+  }
+  ## this mean we can overwrite latest,dev, or whatever tags but can't v* tags like v1.0.0 v1.3.0 likewhise
+
+  encryption_configuration {
+    encryption_type = "AES265"
+  }
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Enviroment = "Production"
+  }
+}
