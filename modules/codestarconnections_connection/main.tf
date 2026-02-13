e7e7a56f072a402c222091af69168632a2cbd415
AkalankaDilshan <akalankadilshan98@gmail.com>
2026-02-13 13:36:19 +0530

    fix: connector provider issue

--- modules/codestarconnections_connection/main.tf
+++ modules/codestarconnections_connection/main.tf
@@ -1,4 +1,10 @@
+# resource "aws_codestarconnections_connection" "github_connector" {
+#   name = var.connector_name
+#   provider = "github"
+# }
+
 resource "aws_codestarconnections_connection" "github_connector" {
-  name = var.connector_name
-  provider = "github"
+  name          = var.connector_name
+  provider_type = "GitHub"
+  provider      = aws  # or aws.your_alias if using an alias
 }
